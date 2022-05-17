4a9315e81cd7e67214227419afd3deb6aa1b5e6a
MaciejIwan <64867638+MaciejIwan@users.noreply.github.com>
2022-05-17 16:06:58 +0200

    start window on center

--- src/main/java/main/Game.java
+++ src/main/java/main/Game.java
@@ -3,9 +3,11 @@ package main;
 import bigBoard.BigBoard;
 import gameInfo.gameInfo;
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.FlowPane;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import java.util.Objects;
 
@@ -15,7 +17,7 @@ public class Game extends Application {
     public Parent createContent(){
         FlowPane root = new FlowPane();
 
-        root.setPrefSize(1400, 900);
+        root.setPrefSize(1400, 950);
 
         gameInfo info = new gameInfo();
         BigBoard bigBoard = new BigBoard(info);
@@ -23,19 +25,29 @@ public class Game extends Application {
         // DODAJ BIG BOARD I GAME INFO DO FLOWPANE
         root.getChildren().add(bigBoard);
         root.getChildren().add(info);
+
+
         return root;
     }
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage primaryStage) {
         Parent root = createContent();
         Scene scene = new Scene(root);
         String css = Objects.requireNonNull(this.getClass().getResource("/main/style.css")).toExternalForm();
         scene.getStylesheets().add(css);
 
-        stage.setTitle("Ultimate Tic Tac Toe!");
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setTitle("Ultimate Tic Tac Toe!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+
+        primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        Rectangle2D primScreenBounds = Screen.getPrimary().getVisualBounds();
+        primaryStage.setX((primScreenBounds.getWidth() - primaryStage.getWidth()) / 2);
+        primaryStage.setY((primScreenBounds.getHeight() - primaryStage.getHeight()) / 2);
     }
 
     public static void main(String[] args) {
